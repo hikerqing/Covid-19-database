bf35fe5b61aeb616a906157c2e3215f64ee1b97a
ZechangSun <60177057+ZechangSun@users.noreply.github.com>
2020-05-27 23:18:27 +0800

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -4,16 +4,16 @@ create Covid-19 database
 */
 CREATE TABLE drug
 (
-    drug_id int,
-    drug_name varchar(50),
-    drug_status varchar(25),
+	drug_id int,
+	drug_name varchar(50),
+	drug_status varchar(25),
     drug_type varchar(25),
     primary key (drug_id)
 );
 
 CREATE TABLE clinical_trial
 (
-    trial_id int,
+	trial_id int,
     title varchar(50),
     trial_type varchar(25),
     trial_stage varchar(25),
@@ -28,8 +28,8 @@ CREATE TABLE clinical_trial
 
 CREATE TABLE trial_drug
 (
-    trial_id int,
-    drug_id int,
+	trial_id int,
+	drug_id int,
     primary key(trial_id),
     foreign key(trial_id) references clinical_trial(trial_id),
     foreign key(drug_id) references drug(drug_id)
@@ -37,7 +37,7 @@ CREATE TABLE trial_drug
 
 create table researcher
 (
-    researcher_id int,
+	researcher_id int,
     first_name varchar(25),
     last_name varchar(25),
     position varchar(25),
@@ -49,7 +49,7 @@ create table researcher
 
 create table institution
 (
-    institution_id int,
+	institution_id int,
     institution_location varchar(50),
     instituiton_money int,
     institution_email varchar(25),
@@ -59,8 +59,8 @@ create table institution
 
 create table institution_researcher
 (
-    researcher_id int,
-    institution_id int,
+	researcher_id int,
+	institution_id int,
     primary key (researcher_id),
     foreign key(researcher_id) references researcher(researcher_id),
     foreign key(institution_id) references institution(institution_id)
@@ -68,7 +68,7 @@ create table institution_researcher
 
 create table experiment
 (
-    trial_id int,
+	trial_id int,
     batch_id int,
     experiment_description varchar(100),
     start_date date,
@@ -83,9 +83,9 @@ create table experiment
 
 create table researcher_experiment
 (
-    researcher_id int,
+	researcher_id int,
     trial_id int,
-    batch_id int,
+	batch_id int,
     division varchar(20),
     primary key(researcher_id),
     foreign key(researcher_id) references researcher(researcher_id),
@@ -129,7 +129,7 @@ CREATE TABLE hospital
     hospital_id int,
     site_id int,
     hospital_name VARCHAR(20),
-    PRIMARY KEY (hospital_id, site_id),
+    PRIMARY KEY (hospital_id,site_id),
     FOREIGN KEY (site_id) REFERENCES SITE(site_id)
 );
 
@@ -164,4 +164,127 @@ CREATE TABLE cases
     left_date date,
     PRIMARY KEY (case_id, site_id),
     FOREIGN KEY (site_id) REFERENCES SITE(site_id) 
+);
+
+CREATE TABLE close_contact
+(
+    site_id int,
+    close_contact_id int,
+    first_name varchar(20),
+    last_name varchar(20),
+    gender VARCHAR(5),
+    age int,
+    status varchar(10),
+    primary key (site_id, close_contact_id),
+    foreign key(site_id) references SITE(site_id)
+);
+
+CREATE Table contact
+(
+    case_site_id int,
+    case_id int,
+    close_contact_site_id int,
+    close_contact_id int,
+    relation VARCHAR(20),
+    contact_date date,
+    contact_place_id int,
+    contact_form varchar(20),
+    primary key (case_site_id, case_id, close_contact_site_id, close_contact_id),
+    foreign key (case_site_id, case_id) references cases(site_id, case_id),
+    foreign key (close_contact_site_id, close_contact_id) references close_contact(site_id, close_contact_id)
+);
+
+CREATE TABLE case_move
+(
+    site_id int,
+    case_id int,
+    start_id int,
+    depart_date date,
+    destination_id int,
+    arrive_date date,
+    vehicle varchar(50),
+    PRIMARY KEY (site_id, case_id, start_id, depart_date, destination_id, arrive_date),
+    FOREIGN KEY (site_id, case_id) REFERENCES cases(site_id, case_id),
+    FOREIGN KEY (start_id) REFERENCES SITE(site_id),
+    FOREIGN KEY (destination_id) REFERENCES SITE(site_id)
+);
+
+CREATE TABLE close_contact_move
+(
+    site_id int,
+    close_contact_id int,
+    start_id int,
+    depart_date date,
+    destination_id int,
+    arrive_date date,
+    vehicle varchar(50),
+    PRIMARY KEY (site_id, close_contact_id, start_id, depart_date, destination_id, arrive_date),
+    FOREIGN KEY (site_id, close_contact_id) REFERENCES close_contact(site_id, close_contact_id),
+    FOREIGN KEY (start_id) REFERENCES SITE(site_id),
+    FOREIGN KEY (destination_id) REFERENCES SITE(site_id)
+);
+
+CREATE TABLE treat_method
+(
+    treat_id int,
+    doctor_id int,
+    doctor_site_id int,
+    doctor_hospital_id int,
+    case_id int,
+    case_site_id int,
+    treat_method VARCHAR(20),
+    start_date date,
+    end_date date,
+    result VARCHAR(50),
+    PRIMARY KEY (treat_id),
+    FOREIGN KEY (doctor_id, doctor_site_id, doctor_hospital_id) REFERENCES doctor(doctor_id, site_id, hospital_id),
+    FOREIGN key (case_id, case_site_id) references cases(case_id, site_id)
+);
+
+CREATE TABLE cases_experiment
+(
+    case_id int,
+    case_site_id int,
+    trial_id int,
+    batch_id int,
+    PRIMARY KEY (case_id, case_site_id, trial_id, batch_id),
+    FOREIGN KEY (case_id, case_site_id) REFERENCES cases (case_id, site_id),
+    FOREIGN KEY (trial_id, batch_id) REFERENCES experiment(trial_id, batch_id)
+);
+
+CREATE TABLE symptom
+(
+    symptom_id int,
+    symptom_name VARCHAR(20),
+    symptom_description VARCHAR(100),
+    PRIMARY KEY (symptom_id)
+);
+
+CREATE TABLE case_develop
+(
+    site_id int,
+    case_id int,
+    develop_date date,
+    symptom_id int,
+    PRIMARY KEY (site_id, case_id, develop_date),
+    FOREIGN KEY (site_id, case_id) REFERENCES cases(site_id, case_id),
+    FOREIGN KEY (symptom_id) REFERENCES symptom(symptom_id)
+);
+
+CREATE TABLE comorbidity
+(
+    comorbidity_id int,
+    comorbidity_name VARCHAR(20),
+    comorbidity_description VARCHAR(50),
+    PRIMARY KEY (comorbidity_id)
+);
+
+CREATE TABLE comorbidity_case
+(
+    site_id int,
+    case_id int,
+    comorbidity_id int,
+    PRIMARY KEY (site_id, case_id, comorbidity_id),
+    FOREIGN KEY (site_id, case_id) REFERENCES cases(site_id, case_id),
+    FOREIGN KEY (comorbidity_id) REFERENCES comorbidity(comorbidity_id)
 );
