bc0d1ae27f18a08b58f3443c4d70355e4098e2c0
hikerqing <43776568+hikerqing@users.noreply.github.com>
2020-05-30 10:49:29 +0800

    Update basic_query.py

--- covid_19_database/basic_query.py
+++ covid_19_database/basic_query.py
@@ -1,115 +1,61 @@
-#1、选择两个国家、开始、截止日期、现存确诊人数/疑似病例数/死亡人数/治愈人数/累计确诊人数
-#!这里不考虑复发的情况，并且认为死亡都是在确诊之后
-#第一个国家的确诊人数
-Select count(case_id),city
+#得到某个国家各个省市病例人数
+#sql语句部分定义函数date_num
+CREATE OR REPLACE FUNCTION date_num(DATE_ date)
+  RETURNS table(case_num bigint,
+			   city_name VARCHAR(20)) AS $$
+begin
+return QUERY SELECT count(case_id) as case_num,city
 From cases,SITE
 Where cases.site_id=SITE.site_id
-Where situation=’confirmed’ and confirmed_date<=DATE1
-Group by city
-Having country=’COUNTRY1’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-#第二个国家的确诊人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’confirmed’ and confirmed_date<=DATE1
-Group by city
-Having country=’COUNTRY2’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
+and situation='confirmed' and confirmed_date<DATE_ and country='china'
+Group by city;
+end;$$
+LANGUAGE 'plpgsql';
+#python批量生成sql语句
+import pandas as pd
+def get_date_list(begin_date,end_date):
+    date_list = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start=begin_date, end=end_date))]
+return date_list
+sql_file=open(r'C:\Users\26089\Desktop\update_file.txt','w',encoding='utf-8')#路径根据文件位置确定
+def wsql(begin_date,end_date):
+for i in get_date_list(begin_date,end_date):
+print(i)
+str_i=str(i)
+line="select * from date_num('%s');"%i
+    sql_file.write(line+'\n')
+wsql('DATE1','DATE2')
+sql_file.close()
 
-#第一个国家的疑似人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’suspected’ and suspected_date<=DATE1
-Group by city
-Having country=’COUNTRY1’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-#第二个国家的疑似人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’suspected’ and suspected_date<=DATE1
-Group by city
-Having country=’COUNTRY2’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-
-#第一个国家日期的死亡人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’dead’ and left_date<=DATE1
-Group by city
-Having country=’COUNTRY1’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-#第二个国家的死亡人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’dead’ and left_date<=DATE1
-Group by city
-Having country=’COUNTRY2’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-
-#第一个国家的治愈人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’cured’ and left_date<=DATE1
-Group by city
-Having country=’COUNTRY1’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-#第二个国家的治愈人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where situation=’cured’ and left_date<=DATE1
-Group by city
-Having country=’COUNTRY2’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-
-#第一个国家的累计确诊人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where (situation=’confirmed’ and confirmed_date<=DATE1)
-Or (situation=’dead’ and left_date<=DATE1)
-Or (situation=’cured’ and left_date<=DATE1)
-Group by city
-Having country=’COUNTRY1’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-#第二个国家的累计确诊人数
-Select count(case_id),city
-From cases,SITE
-Where cases.site_id=SITE.site_id
-Where (situation=’confirmed’ and confirmed_date<=DATE1)
-Or (situation=’dead’ and left_date<=DATE1)
-Or (situation=’cured’ and left_date<=DATE1)
-Group by city
-Having country=’COUNTRY2’
-Where DATE in (select *
-From generate_series(‘DATE1’::timestamp,’DATE2’,’24 hours’))
-
-#某个国家某类药物、开始、截止日期、药物研发情况
-With researcher_institution(researcher_id,trial_id,institution_id,institution_location)
+#得到某个类型药物的研发情况
+#sql语句定义函数drug_num
+CREATE OR REPLACE FUNCTION drug_num(DATE_ date)
+  RETURNS table(insti_location VARCHAR(20),
+			   drug_count bigint) AS $$
+begin
+return QUERY With researcher_institution(researcher_id,trial_id,institution_id,institution_location)
 As (select researcher_id,trial_id,institution_id,institution_location
 From researcher_experiment natural join institution natural join institution_researcher natural join experiment
-Where start_date<=DATE and end_date>=DATE),
+Where start_date<=DATE_ and end_date>=DATE_),
 Drug_trial(trial_id,drug_id,drug_type)
 As (select trial_id,drug_id,drug_type
 From trial_drug natural join drug)
-Select institution_loction,count(drug_id)
-From researcher_institution natural join trial_drug
-Where drug_type=’TYPE’
-Group by institution_location
-Where DATE in (select *
-From generate_series(‘DATA1’’::timestamp,’DATE2’,’24hours’))
+Select institution_location,count(drug_id) as drug_count
+From researcher_institution natural join drug_trial
+Where drug_type='TYPE'
+group by institution_location;
+end;$$
+LANGUAGE 'plpgsql';
+#python批量生成sql语句
+import pandas as pd
+def get_date_list(begin_date,end_date):
+    date_list = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start=begin_date, end=end_date))]
+return date_list
+sql_file=open(r'C:\Users\26089\Desktop\update_file.txt','w',encoding='utf-8')#路径根据文件位置确定
+def wsql(begin_date,end_date):
+for i in get_date_list(begin_date,end_date):
+print(i)
+str_i=str(i)
+line="select * from drug_num('%s');"%i
+    sql_file.write(line+'\n')
+wsql('DATE1','DATE2')
+sql_file.close()
